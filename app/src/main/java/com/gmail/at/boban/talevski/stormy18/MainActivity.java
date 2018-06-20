6aaa7626a67f9930efaed601955b53b002c8a1c7
Boban Talevski <talevski.boban@gmail.com>
2018-06-21 01:54:22 +0200

    Finished Making an HTTP GET Request with OkHttp video

--- app/src/main/java/com/gmail/at/boban/talevski/stormy18/MainActivity.java
+++ app/src/main/java/com/gmail/at/boban/talevski/stormy18/MainActivity.java
@@ -2,12 +2,45 @@ package com.gmail.at.boban.talevski.stormy18;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastUrl = "https://api.darksky.net/forecast/"
+                + Constants.API_KEY + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "IO Excpection caught: ", e);
+        }
     }
 }
