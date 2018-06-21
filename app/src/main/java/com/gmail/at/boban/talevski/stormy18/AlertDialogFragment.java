0a0461d584a9153b8294e48076256048414c01b6
Boban Talevski <talevski.boban@gmail.com>
2018-06-21 19:03:18 +0200

    Finished Configuring the AlertDialog video

--- app/src/main/java/com/gmail/at/boban/talevski/stormy18/AlertDialogFragment.java
+++ app/src/main/java/com/gmail/at/boban/talevski/stormy18/AlertDialogFragment.java
@@ -11,5 +11,11 @@ public class AlertDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Context context = getActivity();
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_button_ok_text, null);
+
+        return builder.create();
     }
 }
--- app/src/main/java/com/gmail/at/boban/talevski/stormy18/MainActivity.java
+++ app/src/main/java/com/gmail/at/boban/talevski/stormy18/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void alertUserAboutError() {
-
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
