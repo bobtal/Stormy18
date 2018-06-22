b17149cb1006606b43c5521d019468d411138bfd
Boban Talevski <talevski.boban@gmail.com>
2018-06-22 02:31:47 +0200

    Finished The Legal Side of APIs video

--- app/src/main/java/com/gmail/at/boban/talevski/stormy18/MainActivity.java
+++ app/src/main/java/com/gmail/at/boban/talevski/stormy18/MainActivity.java
@@ -5,7 +5,9 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView darkSky = findViewById(R.id.darkSkyAttribution);
+        darkSky.setMovementMethod(LinkMovementMethod.getInstance());
 
         double latitude = 37.8267;
         double longitude = -122.4233;
