ada82cec306f4b80a1df8a725e5506f18da5c91f
Boban Talevski <talevski.boban@gmail.com>
2018-06-23 03:04:15 +0200

    Finished Adapting the Adapter video

--- app/src/main/java/com/gmail/at/boban/talevski/stormy18/adapters/HourlyAdapter.java
+++ app/src/main/java/com/gmail/at/boban/talevski/stormy18/adapters/HourlyAdapter.java
@@ -1,28 +1,49 @@
 package com.gmail.at.boban.talevski.stormy18.adapters;
 
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.gmail.at.boban.talevski.stormy18.R;
+import com.gmail.at.boban.talevski.stormy18.Weather.Hour;
 import com.gmail.at.boban.talevski.stormy18.databinding.HourlyListItemBinding;
+
+import java.util.List;
 
 public class HourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.ViewHolder> {
 
+    private List<Hour> hours;
+    private Context context;
+
+    public HourlyAdapter(List<Hour> hours, Context context) {
+        this.hours = hours;
+        this.context = context;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public HourlyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        HourlyListItemBinding binding = DataBindingUtil
+                .inflate(LayoutInflater.from(parent.getContext()),
+                        R.layout.hourly_list_item, parent,
+                        false);
+        return new ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Hour hour = hours.get(position);
+        holder.hourlyListItemBinding.setHour(hour);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return hours.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
